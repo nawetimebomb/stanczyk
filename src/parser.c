fbd05435b1b0ec669798d369cb36aeb6270ae0d2
Nahuel J. Sacchetti <git@nsacchetti.com>
2023-05-12 00:37:52 -0500

    feat: add load procedure

--- src/parser.c
+++ src/parser.c
@@ -4,6 +4,14 @@
 #include "nexp.h"
 #include "types.h"
 
+static mpc_parser_t *_number;
+static mpc_parser_t *_symbol;
+static mpc_parser_t *_string;
+static mpc_parser_t *_comment;
+static mpc_parser_t *_Sexpr;
+static mpc_parser_t *_Bexpr;
+static mpc_parser_t *_expr;
+
 static nexp_t *parse_number(mpc_ast_t *ast) {
     errno = 0;
     i64 result = strtol(ast->contents, NULL, 10);
@@ -21,6 +29,30 @@ static nexp_t *parse_string(mpc_ast_t *ast) {
     nexp_t *string = nexp_new_string(unescaped);
     free(unescaped);
     return string;
+}
+
+void init_parser() {
+    _number  = mpc_new("number");
+    _symbol  = mpc_new("symbol");
+    _string  = mpc_new("string");
+    _comment = mpc_new("comment");
+    _Sexpr   = mpc_new("Sexpr");
+    _Bexpr   = mpc_new("Bexpr");
+    _expr    = mpc_new("expr");
+}
+
+mpc_parser_t *get_parser_type(parser_type_t parser_type) {
+    switch (parser_type) {
+        case NEXP_PARSER_TYPE_NUMBER:  return _number;
+        case NEXP_PARSER_TYPE_SYMBOL:  return _symbol;
+        case NEXP_PARSER_TYPE_STRING:  return _string;
+        case NEXP_PARSER_TYPE_COMMENT: return _comment;
+        case NEXP_PARSER_TYPE_SEXPR:   return _Sexpr;
+        case NEXP_PARSER_TYPE_BEXPR:   return _Bexpr;
+        case NEXP_PARSER_TYPE_EXPR:    return _expr;
+    }
+
+    return _number;
 }
 
 nexp_t *parse_expr(mpc_ast_t *ast) {
