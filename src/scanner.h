df4a65055de0962364dc88f3ad233a1a262af841
Nahuel J. Sacchetti <me@nsacchetti.com>
2023-06-15 15:40:25 -0500

    feat: code cleanup and documentation

--- src/scanner.h
+++ src/scanner.h
@@ -2,51 +2,55 @@
 #define NLISP_SCANNER_H
 
 typedef enum {
-  // Single & double character tokens
-  TOKEN_LEFT_PAREN,
-  TOKEN_RIGHT_PAREN,
-  TOKEN_LEFT_BRACKET,
-  TOKEN_RIGHT_BRACKET,
-  TOKEN_COMMA,
-  TOKEN_DOT,
-  TOKEN_MINUS,
-  TOKEN_PLUS,
-  TOKEN_COLON,
-  TOKEN_EQUAL,
-  TOKEN_SLASH,
-  TOKEN_STAR,
-  TOKEN_BANG,
-  TOKEN_BANG_EQUAL,
-  TOKEN_EQUAL_EQUAL,
-  TOKEN_GREATER,
-  TOKEN_GREATER_EQUAL,
-  TOKEN_LESS,
-  TOKEN_LESS_EQUAL,
+    // Single char tokens
+    TOKEN_LEFT_PAREN,    // (
+    TOKEN_RIGHT_PAREN,   // )
+    TOKEN_LEFT_BRACE,    // {
+    TOKEN_RIGHT_BRACE,   // }
+    TOKEN_LEFT_BRACKET,  // [
+    TOKEN_RIGHT_BRACKET, // ]
+    TOKEN_COMMA,
+    TOKEN_DOT,
+    TOKEN_MINUS,
+    TOKEN_PLUS,
+    TOKEN_SLASH,
+    TOKEN_STAR,
+    TOKEN_COLON,
+    TOKEN_EQUAL,
+    TOKEN_BANG,
+    TOKEN_GREATER,
+    TOKEN_LESS,
 
-  // Literals
-  TOKEN_SYMBOL,
-  TOKEN_STRING,
-  TOKEN_FLOAT,
-  TOKEN_INT,
+    // Double char tokens
+    TOKEN_BANG_EQUAL,
+    TOKEN_EQUAL_EQUAL,
+    TOKEN_GREATER_EQUAL,
+    TOKEN_LESS_EQUAL,
 
-  // Keywords
-  TOKEN_AND,
-  TOKEN_DO,
-  TOKEN_DROP,
-  TOKEN_DUP,
-  TOKEN_ELSE,
-  TOKEN_FALSE,
-  TOKEN_IF,
-  TOKEN_NIL,
-  TOKEN_OR,
-  TOKEN_NEG,
-  TOKEN_PRINT,
-  TOKEN_QUIT,
-  TOKEN_TRUE,
+    // Literals
+    TOKEN_SYMBOL,
+    TOKEN_STRING,
+    TOKEN_FLOAT,
+    TOKEN_INT,
 
-  // Special
-  TOKEN_ERROR,
-  TOKEN_EOF
+    // Keywords
+    TOKEN_AND,
+    TOKEN_DO,
+    TOKEN_DROP,
+    TOKEN_DUP,
+    TOKEN_ELSE,
+    TOKEN_FALSE,
+    TOKEN_IF,
+    TOKEN_NIL,
+    TOKEN_OR,
+    TOKEN_NEG,
+    TOKEN_PRINT,
+    TOKEN_QUIT,
+    TOKEN_TRUE,
+
+    // Special
+    TOKEN_ERROR,
+    TOKEN_EOF
 } token_type_t;
 
 typedef struct {
