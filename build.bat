a29f58675d659f8f7964468173285a19e66028c9
Nahuel J. Sacchetti <me@nsacchetti.com>
2025-03-03 23:48:29 -0600

    feat: start with Odin

--- build.bat
+++ build.bat
@@ -1,8 +1,12 @@
 @echo off
 
-set DestinationFolder=".\bin"
 set ExecutableName="skc.exe"
-set SourceFolder="compiler"
+set DestinationFolder=".\bin"
+set Command="%1"
+
+if %Command% == "clean" (
+   DEL /S /Q %DestinationFolder%
+)
 
 IF NOT EXIST %DestinationFolder% (
    MKDIR %DestinationFolder%
@@ -10,8 +14,16 @@ IF NOT EXIST %DestinationFolder% (
 
 PUSHD %DestinationFolder%
 
-odin build ..\%SourceFolder% -use-separate-modules -out:%ExecutableName% -strict-style -vet-using-stmt -vet-using-param -vet-style -vet-semicolon -debug
+odin build ../src -show-timings -use-separate-modules -out:%ExecutableName% -strict-style -vet-using-stmt -vet-using-param -vet-style -vet-semicolon -debug
+rem -subsystem:windows
+
+IF %ERRORLEVEL% NEQ 0 (
+   POPD
+   EXIT /b 1
+)
+
+IF %Command% == "run" (
+   CALL %ExecutableName%
+)
 
 POPD
-
-exit /b 0
